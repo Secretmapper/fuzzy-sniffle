a827f24514d1482b5048c2d55f855fa816c858f7
Arian Valdez <Secretmapper16@gmail.com>
2020-11-03 10:56:37 +0800

    Update frontpage

--- src/components/FrontPage/FrontPage.js
+++ src/components/FrontPage/FrontPage.js
@@ -36,7 +36,11 @@ class FrontPage extends Component {
           <p>
             Data gathered and displayed here are curated by: <br />
             <br />
-            <b>Dr. Vena Pearl Bongolan</b>, <i>College of Engineering</i> <br />
+            <b>
+              <a href='https://sites.google.com/a/dcs.upd.edu.ph/sms/vpbongolan'>
+                Dr. Vena Pearl Bongolan
+              </a>
+            </b>, <i>College of Engineering</i> <br />
             <b>Dr. Rizalino B. Cruz</b>,{" "}
             <i>
               National College of Public Administration and Governance (NCPAG)
@@ -45,12 +49,21 @@ class FrontPage extends Component {
             <b>Prof. Roselle Leah K. Rivera</b>,{" "}
             <i>College of Social Work and Community Development</i>
             <br />
-            <b>Nikki Fernandez</b> and <b>Carlo La Rosa</b>,{" "}
-            <i>Undergraduate Students (2019)</i> <br />
-            <b>Wystan Concepcion </b> and{" "}
-            <b>James Gabriel Abaja </b>, <i>Undergraduate Students (2018)</i><br />{" "}
             <br />
-            
+            <p>
+              <b>Contact Us / Consultation Availability:</b>
+              <p>
+                <a href='https://sites.google.com/a/dcs.upd.edu.ph/sms/vpbongolan'>
+                  Dr. Vena Pearl Bongolan
+                </a>
+                {' '} <b>(vabongolan@up.edu.ph)</b> - Tuesday, Thursday: 2:40-6pm
+              </p> 
+              <p>
+                <a href='https://arianv.com'>
+                  Arian Allenson Valdez 
+                </a>
+                {' '} <b>(me@arianv.com)</b> - Wednesday, Friday: 6pm - 8pm</p> 
+            </p>
           </p>
           <p>
             <b>Analysis is exclusively for the Philippine setting</b>, and
@@ -64,23 +77,6 @@ class FrontPage extends Component {
           <p>
             For a background on our analysis and methods, your may refer to the
             study by{" "}
-            <a
-              href="https://council.science/publications/a-guide-to-sdg-interactions-from-science-to-implementation"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              clicking this link.
-            </a>
-          </p>
-          <p>
-            For an overview of the study by Abaja and Concepcion <i>(2018)</i>,{" "}
-            <a
-              href="https://drive.google.com/file/d/1u9ZQ0W_8WY7rtauS0xNbGvTvx0QLl2KR/view"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              click this link to view the poster.
-            </a>
           </p>
           <p className="lead">
             <i>
@@ -88,8 +84,33 @@ class FrontPage extends Component {
               on compatibility issues with FireFox.
             </i>
           </p>
-          <p><i>Contact us</i> <br />
-            <b>Nikki Fernandez</b> (nrfernandez2@up.edu.ph) and <b>Carlo La Rosa</b> (larosacarlo18@gmail.com)</p>
+          <p>
+            <b>Current Undergraduate Student</b>
+            <br />
+            <b>Arian Allenson Valdez</b>, <i> Undergraduate Student 2020</i>
+            <br />
+            <br />
+            <b>Previous Undergraduate Students</b>
+            <br />
+            <a
+              href="https://council.science/publications/a-guide-to-sdg-interactions-from-science-to-implementation"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+            <b>Nikki Fernandez</b> and <b>Carlo La Rosa</b>,{" "}
+            </a>
+            <i>Undergraduate Students (2019)</i> <br />
+            <a
+              href="https://drive.google.com/file/d/1u9ZQ0W_8WY7rtauS0xNbGvTvx0QLl2KR/view"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <b>Wystan Concepcion </b> and{" "}
+              <b>James Gabriel Abaja </b>
+            </a>
+            ,<i>Undergraduate Students (2018)</i><br />{" "}
+            <br />
+          </p>
           <hr className="my-2" />
           <p className="lead">
             <Button outline color="primary" href="/signup">
